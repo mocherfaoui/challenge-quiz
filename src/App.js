5fd54133a35c2636582922a61539164138755966
Mohammed <hi@cherfaoui.dev>
2023-02-11 19:26:31 +0100

    refactor: move styled-provider

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from "react";
-import styled, { ThemeProvider } from "styled-components";
-import Question from "./components/question";
-
-import questions from "./questions.json";
-import { getQuestions } from "./utils/get-questions";
-
-const theme = {
-  colors: {
-    gray1: "hsl(0, 0%, 99.0%)",
-    gray2: "hsl(0, 0%, 97.3%)",
-    gray3: "hsl(0, 0%, 95.1%)",
-    gray4: "hsl(0, 0%, 93.0%)",
-    gray5: "hsl(0, 0%, 90.9%)",
-    gray6: "hsl(0, 0%, 88.7%)",
-    gray7: "hsl(0, 0%, 85.8%)",
-    gray8: "hsl(0, 0%, 78.0%)",
-    gray9: "hsl(0, 0%, 56.1%)",
-    gray10: "hsl(0, 0%, 52.3%)",
-    gray11: "hsl(0, 0%, 43.5%)",
-    gray12: "hsl(0, 0%, 9.0%)",
-  },
-};
+import styled from "styled-components";
+import Quiz from "./components/quiz";
+import { StyledProvider } from "./providers/styled-provider";
 
 function App() {
-  const parsedQuestions = getQuestions(questions);
-
   return (
-    <ThemeProvider theme={theme}>
+    <StyledProvider>
       <Background>
         <Wrapper>
-          <Question questions={parsedQuestions} />
+          <Quiz />
         </Wrapper>
       </Background>
-    </ThemeProvider>
+    </StyledProvider>
   );
 }
 
