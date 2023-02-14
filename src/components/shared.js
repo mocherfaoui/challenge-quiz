0827b105b97a6e0fb0d8a036ce1b213f5deadd9c
Mohammed <hi@cherfaoui.dev>
2023-02-14 20:05:34 +0100

    refactor: move reused components to shared

---
+++ src/components/shared.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-weight: 500;
+  font-size: 1rem;
+  border-radius: 4px;
+  border-width: 1px;
+  border-style: solid;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+`;
